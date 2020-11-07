ef6f1b803bc1d1c7654dfda2b5b5121347667997
cmhxone <cmhxone@outlook.kr>
2020-11-07 20:52:57 +0900

    Add SDL2_Mixer

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use physics::velocity::Velocity;
 // FPS 값
 const FRAME_PER_SECOND: u32 = 60;
 // 화면 크기
-const SCREEN_SIZE: (u32, u32) = (1920, 1080);
+const SCREEN_SIZE: (u32, u32) = (1280, 720);
 // 스프라이트의 크기 상수
 const SPRITE_TILE_SIZE: (u32, u32) = (64, 64);
 // 플레이어 걷기 애니메이션의 최대 스프라이트 갯수
@@ -62,12 +62,13 @@ pub fn main() {
         // 게임루프 시작 틱(ms)
         let start_tick = timer.ticks();
 
-        canvas.clear();
-
+        
         for event in event_pump.poll_iter() {
+            canvas.clear();
             match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                // Event::Quit {..} |
+                // Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                Event::Quit {..} => {
                     break 'running
                 },
                 _ => {}
